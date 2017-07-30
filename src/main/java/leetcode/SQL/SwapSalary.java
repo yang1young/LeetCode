0853bd15e04d39c369dc23cdd58b01387b6d9623
yang1young <1031545274@qq.com>
2017-07-30 20:24:07 +0800

    add new sql solution

---
+++ src/main/java/leetcode/SQL/SwapSalary.java
@@ -0,0 +1,29 @@
+package leetcode.SQL;
+
+/**
+ * Created by yang on 17-7-30.
+ *
+ Given a table salary, such as the one below, that has m=male and f=female values. Swap all f and m values (i.e., change all f values to m and vice versa) with a single update query and no intermediate temp table.
+ For example:
+
+ | id | name | sex | salary |
+ |----|------|-----|--------|
+ | 1  | A    | m   | 2500   |
+ | 2  | B    | f   | 1500   |
+ | 3  | C    | m   | 5500   |
+ | 4  | D    | f   | 500    |
+
+ After running your query, the above salary table should have the following rows:
+
+ | id | name | sex | salary |
+ |----|------|-----|--------|
+ | 1  | A    | f   | 2500   |
+ | 2  | B    | m   | 1500   |
+ | 3  | C    | f   | 5500   |
+ | 4  | D    | m   | 500    |
+
+ */
+public class SwapSalary {
+    //UPDATE salary SET sex = IF(sex = 'm', 'f', 'm')
+    //UPDATE salary SET sex  = (CASE WHEN sex = 'm' THEN  'f' ELSE 'm' END)
+}
